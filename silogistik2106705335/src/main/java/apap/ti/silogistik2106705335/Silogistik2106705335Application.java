956bbfaf137a6cd42c57c48b6594cce73047d3a4
maureenesthw <maureen.esther.wijaya@gmail.com>
2023-09-30 23:37:25 +0700

    project: initial project

---
+++ silogistik2106705335/src/main/java/apap/ti/silogistik2106705335/Silogistik2106705335Application.java
@@ -0,0 +1,13 @@
+package apap.ti.silogistik2106705335;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Silogistik2106705335Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Silogistik2106705335Application.class, args);
+	}
+
+}
---
+++ silogistik2106705335/src/test/java/apap/ti/silogistik2106705335/Silogistik2106705335ApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.ti.silogistik2106705335;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Silogistik2106705335ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
