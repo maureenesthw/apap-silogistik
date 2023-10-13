82cc99ebebfbc50eb39903a84f667826f56fbe9f
maureenesthw <maureen.esther.wijaya@gmail.com>
2023-10-14 00:46:01 +0700

    ui: check ui done

--- silogistik2106705335/src/main/resources/static/js/datatables.js
+++ silogistik2106705335/src/main/resources/static/js/datatables.js
@@ -1,12 +1,7 @@
-$(document).ready( function () {
-    $('#myTable').DataTable({
-        lengthMenu: [5, 10, 20], // Set the available options for the number of entries per page
-        pageLength: 5, // Set the default number of entries per page
-        "columnDefs": [
-            {
-                "targets": [5], // Replace with the index of the column you want to modify (e.g., 0 for the first column)
-                "orderable": false // Set to false to disable sorting for the specified column
-            }
-        ],
+$(document).ready(function () {
+    $('#gudangTable').DataTable({
+        paging: false,   // Disable pagination
+        searching: false, // Disable search
+        order: [],       // Disable sorting for all columns
     });
-} );
+});
